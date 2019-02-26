3e02350bb9d6ce18e5ec4561051f57f431ce3346
Konto do wykonywani kopii zapasowych <mibackup@mibackup.nencki.gov.pl>
2019-02-26 13:56:44 +0100

    will stay on mriqc:0.12.1 for back compatibility

--- dcm2bids.sh
+++ dcm2bids.sh
@@ -16,8 +16,8 @@ rm -r $orig
 touch $dest/busy
 
 docker run --rm --network none -v /opt/bids/dicoms:/data:ro -v /home/mibackup/bids/heuristic.py:/heuristic.py -v /opt/bids:/output nipy/heudiconv:latest -d /data/{subject}/*/*/*.dcm -s $i -f /heuristic.py -b --overwrite -o /output
-docker run --rm --network none -v /opt/bids:/data:ro -v /opt/bids/mriqc:/out poldracklab/mriqc:latest /data /out participant --participant-label $subjid
-docker run --rm --network none -v /opt/bids:/data:ro -v /opt/bids/mriqc:/out poldracklab/mriqc:latest /data /out group
+docker run --rm --network none -v /opt/bids:/data:ro -v /opt/bids/mriqc:/out poldracklab/mriqc:0.12.1 /data /out participant --participant-label $subjid
+docker run --rm --network none -v /opt/bids:/data:ro -v /opt/bids/mriqc:/out poldracklab/mriqc:0.12.1 /data /out group
 
 cp -r $dest /opt/.trash/
 rm -r $dest
