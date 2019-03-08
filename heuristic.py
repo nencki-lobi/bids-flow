50f7aef458034fc6063c78ae8bf86b8d236d278f
Michał Szczepanik <m.szczepanik@nencki.gov.pl>
2019-03-08 20:32:12 +0100

    Add ability to extract task and run name from protocol name
    If the protocol name matches task-([a-zA-z]+)_run([0-9]+)
    the matching groups will be used as task & run labels in the
    BIDS file name. Otherwise, generic 'task-fmri' rules still apply.
    
    This creates a need to discard runs of a given task that repeated
    (otherwise heudiconv will crash when trying to create a file which
    already exists). A rule has been proposed: leave the run which is
    longest and latest (i. e. sort runs first by dim4 and then by
    series_id). This assumes that series_id starts with a number,
    which should be the case.
    
    This should mitigate a special case of issue #1

--- heuristic.py
+++ heuristic.py
@@ -1,3 +1,40 @@
+import re
+from collections import defaultdict
+
+t_r_regex = re.compile(
+    'task-(?P<tasklabel>[a-zA-Z]+)_run-(?P<runlabel>[0-9]+)')
+
+
+def find_repeated(seqinfo):
+    """Create a list of series_ids from repeated runs
+    Assumes that a run should be excluded on two criterions, in this order:
+    1. if there are longer runs of the given task (sort by dim4)
+       (interrruptions happen, but duration is usually constant)
+    2. if there are subsequent runs of the given task (sort by series_id)
+       (completed run may be repeated by experimenter's decision)
+    """
+    exclusions = []
+
+    # find all sequences that have task & run defined in the name
+    # and collect them into a dictionary (tasklabel, runlabel): [seqinfos]
+    matching = defaultdict(list)
+    for s in seqinfo:
+        match = re.match(t_r_regex, s.protocol_name)
+        if match is not None:
+            matching[match.groups()].append(s)
+
+    # for each task-run pair, pick series_ids to be excluded
+    for s_list in matching.values():
+        if len(s_list) > 1:
+            # more than 1 occurence of task&run
+            # sort by dim4 and then series_id
+            s_list.sort(key=lambda x: (x.dim4, x.series_id))
+            # list all but one for exclusion
+            for i in range(len(s_list) - 1):
+                exclusions.append(s_list[i].series_id)
+    return exclusions
+
+
 def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
         raise ValueError('Template must be a valid format string')
@@ -17,6 +54,9 @@ def infotodict(seqinfo):
 
     t1w = create_key(
         'sub-{subject}/anat/sub-{subject}_T1w')
+    task_run = create_key(
+        'sub-{subject}/func/sub-{subject}_task-{tasklabel}_run-{runlabel}'
+        '_bold')
     bold = create_key(
         'sub-{subject}/func/sub-{subject}_task-fmri_run-{item:01d}_bold')
     rest = create_key(
@@ -27,9 +67,19 @@ def infotodict(seqinfo):
         'sub-{subject}/fmap/sub-{subject}_magnitude')
 
     info = {t1w: [], rest: [], bold: [], fmap_phasediff: [],
-            fmap_magnitude: []}
+            fmap_magnitude: [], task_run: []}
+
+    # check if any task-<>_run-<> repeats and should be excluded
+    exclusions = find_repeated(seqinfo)
 
     for idx, s in enumerate(seqinfo):
+
+        t_r_match = re.match(t_r_regex, s.protocol_name)  # task-<>_run-<>
+
+        if s.series_id in exclusions:
+            #  todo: think of some logging
+            continue
+
         # s is a namedtuple with fields equal to the names of the columns
         # found in dicominfo.txt
         if ('t1_mpr' in s.protocol_name) or ('T1w' in s.protocol_name):
@@ -41,6 +91,12 @@ def infotodict(seqinfo):
         elif s.protocol_name == 'gre_field_mapping' and s.image_type[2] == 'P':
             print("omit FM")
             # info[fmap_phasediff].append(s.series_id)
+        elif t_r_match is not None:
+            info[task_run].append(
+                {'item': s.series_id,
+                 'tasklabel': t_r_match.group('tasklabel'),
+                 'runlabel': t_r_match.group('runlabel')
+                 })
         elif ('rest' in s.protocol_name):
             info[rest].append(s.series_id)
         elif ('ep2d_bold' in s.protocol_name) or ('task' in s.protocol_name):
