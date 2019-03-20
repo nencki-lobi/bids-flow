be58806c8d448f5d9c04282eb53aae266232d6af
mibackup <mibackup@mibackup.nencki.gov.pl>
2019-03-20 22:09:17 +0100

    bxh/xcede tools for phantom QA

---
+++ bxh-xcede/run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Configure PATH
+export PATH=$PATH:/opt/bxh_xcede_tools-1.11.14-lsb30.x86_64/bin
+source /etc/afni/afni.sh
+
+cd /home/input
+#1 remove spaces
+#2 convert to bxh
+dicom2bxh *.dcm dicoms.bxh
+#3 analyze
+csh -c "fmriqa_phantomqa.pl dicoms.bxh fmriqa"
