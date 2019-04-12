7d33dafdee3ef5b00d9450f3ca098cfc226afc6e
Michał Szczepanik <m.szczepanik@nencki.gov.pl>
2019-04-12 12:41:32 +0200

    Add --minmeta to heudiconv call
    As per docs, --minmeta flag restricts the json file to only BIDS keys.
    I noticed that (when running on my PC) multi-echo files don't get
    converted properly without this flag (some arcane things going on,
    apparently).

--- bids_apps.sh
+++ bids_apps.sh
@@ -2,6 +2,6 @@
 
 subjid=${1//_/}
 
-docker run --rm --network none -v /opt/bids/dicoms:/data:ro -v /home/mibackup/bids/heuristic.py:/heuristic.py -v /opt/bids:/output nipy/heudiconv:latest -d /data/{subject}/*/*/*.dcm -s $1 -f /heuristic.py -b --overwrite -o /output
+docker run --rm --network none -v /opt/bids/dicoms:/data:ro -v /home/mibackup/bids/heuristic.py:/heuristic.py -v /opt/bids:/output nipy/heudiconv:latest -d /data/{subject}/*/*/*.dcm -s $1 -f /heuristic.py -b --minmeta --overwrite -o /output
 docker run --rm --network none -v /opt/bids:/data:ro -v /opt/bids/mriqc:/out poldracklab/mriqc:0.12.1 /data /out participant --participant-label $subjid
 docker run --rm --network none -v /opt/bids:/data:ro -v /opt/bids/mriqc:/out poldracklab/mriqc:0.12.1 /data /out group
