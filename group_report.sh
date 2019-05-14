f6f1d8e0ae3f0a2bef727c69b71eefb296072856
mibackup <mibackup@mibackup.nencki.gov.pl>
2019-05-14 10:51:03 +0200

    group report will work with old reports

--- group_report.sh
+++ group_report.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-cd /opt/bids/mriqc
+#group report is optimised for mriqc 0.12.1
+cd /opt/bids/mriqc_12
 mkdir -p reports/$1
 find derivatives -iname sub-$1* -exec cp --parents {} reports/$1/ \;
 
-docker run --rm --user $(id -u):$(id -g) --network none -v /opt/bids/mriqc/reports/$1:/data:ro -v /opt/bids/mriqc/reports/$1:/out poldracklab/mriqc:0.12.1 /data /out group
+docker run --rm --user $(id -u):$(id -g) --network none -v /opt/bids/mriqc_12/reports/$1:/data:ro -v /opt/bids/mriqc_12/reports/$1:/out poldracklab/mriqc:0.12.1 /data /out group
 rm -rf reports/$1/derivatives
-ln -rs  reports/sub-$1* reports/$1/reports/
+ln -frs  reports/sub-$1* reports/$1/reports/
