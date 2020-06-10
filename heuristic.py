5c5551304df06e1bc5a4cedb45888afd86c0cbab
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2020-06-10 12:38:18 +0200

    Minor PEP-8 stylistic changes

--- heuristic.py
+++ heuristic.py
@@ -47,8 +47,8 @@ def create_key(template, outtype=('nii.gz',), annotation_classes=None):
 
 
 def infotoids(seqinfos, outdir):
-    seqinfos=list(seqinfos)
-    subject= fixup_subjectid(seqinfos[0].patient_id)
+    seqinfos = list(seqinfos)
+    subject = fixup_subjectid(seqinfos[0].patient_id)
     return {
         'subject': subject,
         'session': seqinfos[0].date}
@@ -115,6 +115,7 @@ def infotodict(seqinfo):
         elif ('ep2d_bold' in s.protocol_name) or ('task' in s.protocol_name) or ('FMRI' in s.protocol_name):
             info[bold].append(s.series_id)
     return info
+
 
 def fixup_subjectid(subjectid):
     return re.sub('[-_]', '', subjectid)
